4cc1eebc3e9433884547726d64e6b7c4bb631904
Vlad <vlad2tiger@gmail.com>
2026-07-22 12:27:55 +0300

    eslint hotfixes

--- src/pages/MainPage/components/MainTenDaysSection/MainTenDaysSection.tsx
+++ src/pages/MainPage/components/MainTenDaysSection/MainTenDaysSection.tsx
@@ -1,12 +1,12 @@
-import DetailedForecast from "@components/DetailedForecast/DetailedForecast"
-import styles from "./mainTenDaysSection.module.scss"
+import DetailedForecast from "@components/DetailedForecast/DetailedForecast";
+import styles from "./mainTenDaysSection.module.scss";
 
 const TenDaysForecastSection = () => {
   return (
     <section className={styles.section_wrapper}>
       <DetailedForecast />
     </section>
-  )
-}
+  );
+};
 
-export default TenDaysForecastSection
+export default TenDaysForecastSection;
