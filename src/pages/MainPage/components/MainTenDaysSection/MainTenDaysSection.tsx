13c7d718cf01f2100c14a6d66cf46d68fb54080e
Vlad <vlad2tiger@gmail.com>
2026-07-25 14:54:51 +0300

    eslint fix

--- src/pages/MainPage/components/MainTenDaysSection/MainTenDaysSection.tsx
+++ src/pages/MainPage/components/MainTenDaysSection/MainTenDaysSection.tsx
@@ -1,12 +1,14 @@
 import DetailedForecast from "@components/DetailedForecast/DetailedForecast";
 import styles from "./mainTenDaysSection.module.scss";
-import Arrow from "@assets/icons/arrowMore.svg"
+import Arrow from "@assets/icons/arrowMore.svg";
 
 const TenDaysForecastSection = () => {
   return (
     <section className={styles.section_wrapper}>
       <DetailedForecast />
-      <button>Прогноз на месяц <img src={Arrow} alt="" /></button>
+      <button>
+        Прогноз на месяц <img src={Arrow} alt="" />
+      </button>
     </section>
   );
 };
