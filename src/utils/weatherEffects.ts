88cd9a209fc66899edd6a1fb06c516266df3d0ef
Vlad <vlad2tiger@gmail.com>
2026-08-05 11:12:49 +0300

    task for weatherEffects completed

--- src/utils/weatherEffects.ts
+++ src/utils/weatherEffects.ts
@@ -20,57 +20,44 @@ const weatherMap = {
   thunder: { codes: [95, 96, 99], text: "Гром", icon: thunder },
 };
 
-function findWeather(code: () => number) {
-  const value = code();
-  return Object.values(weatherMap).find((item) => item.codes.includes(value));
+function findWeather(code: number) {
+  return Object.values(weatherMap).find((item) => item.codes.includes(code));
 }
 
-export const getAvgWeatherCode = (arg: number[]) => {
-  const freq: Record<number, number> = {};
-
-  for (const code of arg) {
-    freq[code] = (freq[code] || 0) + 1;
-  }
-
-  const entries = Object.entries(freq).sort((a, b) => b[1] - a[1]);
-
-  return Number(entries[0][0]);
-};
-
-export function getWeatherEffect(code: () => number) {
-  return findWeather(code)?.icon;
+export function getWeatherEffect(code: number) {
+  const weather = findWeather(code);
+  if (!weather) return null;
+  return weather.icon;
 }
 
-export function getWeatherInfo(code: () => number) {
-  return findWeather(code)?.text;
+export function getWeatherInfo(code: number) {
+  const weather = findWeather(code);
+  if (!weather) return null;
+  return weather.text;
 }
 
-export function getRainChance(chance: () => number): string {
-  const value = chance();
-  if (value <= 0) return "осадков не ожидается";
-  if (value <= 20) return "небольшая вероятность осадков";
-  if (value <= 50) return "есть вероятность осадков";
-  if (value <= 80) return "высокая вероятность осадков";
+export function getRainChance(chance: number) {
+  if (chance <= 0) return "осадков не ожидается";
+  if (chance <= 20) return "небольшая вероятность осадков";
+  if (chance <= 50) return "есть вероятность осадков";
+  if (chance <= 80) return "высокая вероятность осадков";
   return "ожидается выпадение осадков";
 }
 
-export function getWindCategory(speed: () => number): string {
-  const value = speed();
-  if (value <= 0.2) return "штиль";
-  if (value <= 5.4) return "слабый ветер";
-  if (value <= 10.7) return "ветер";
-  if (value <= 15.2) return "сильный ветер";
-  if (value <= 24.4) return "шторм";
-  if (value <= 28.4) return "сильный шторм";
+export function getWindCategory(speed: number) {
+  if (speed <= 0.2) return "штиль";
+  if (speed <= 5.4) return "слабый ветер";
+  if (speed <= 10.7) return "ветер";
+  if (speed <= 15.2) return "сильный ветер";
+  if (speed <= 24.4) return "шторм";
+  if (speed <= 28.4) return "сильный шторм";
   return "ураган";
 }
 
-export function getWindDirection(deg: () => number): string {
+export function getWindDirection(deg: number) {
   const directions = ["С", "СВ", "В", "ЮВ", "Ю", "ЮЗ", "З", "СЗ"];
 
-  const value = deg();
-
-  const index = Math.round(value / 45) % 8;
+  const index = Math.round(deg / 45) % 8;
   return directions[index];
 }
 
@@ -102,4 +89,16 @@ export function getUVCategory(category: number) {
   if (category <= 10)
     return { fill: 0.67, color: "#c30101", text: "очень высокий" };
   return { fill: 1, color: "#57348d", text: "экстремальный" };
+}
+
+export function getAvgWeatherCode(arg: number[]) {
+  const freq: Record<number, number> = {};
+
+  for (const code of arg) {
+    freq[code] = (freq[code] || 0) + 1;
+  }
+
+  const entries = Object.entries(freq).sort((a, b) => b[1] - a[1]);
+
+  return Number(entries[0][0]);
 }
