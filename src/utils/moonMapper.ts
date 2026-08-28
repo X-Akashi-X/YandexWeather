543ed003a8cafb628f19dc453065f7ec1737fa0e
Vlad <vlad2tiger@gmail.com>
2026-08-28 10:11:13 +0300

    moon naming fix

--- src/utils/moonMapper.ts
+++ src/utils/moonMapper.ts
@@ -10,12 +10,12 @@ import waningSmall from "@assets/icons/moon/waningSmallIcon.svg";
 export function moonMap(category: number) {
   if (category <= 0.04) return { text: "новолуние", icon: newMoon };
   if (category <= 0.22) return { text: "молодая", icon: waxingSmall };
-  if (category <= 0.28) return { text: "первая четверть", icon: firsthQuarter };
+  if (category <= 0.28) return { text: "первая 1/4", icon: firsthQuarter };
   if (category <= 0.46) return { text: "растущая", icon: waxingBig };
   if (category <= 0.54) return { text: "полнолуние", icon: fullMoon };
   if (category <= 0.72) return { text: "убывающая", icon: waningBig };
   if (category <= 0.78)
-    return { text: "последняя четверть", icon: lastQuarter };
+    return { text: "последняя 1/4", icon: lastQuarter };
   if (category <= 0.96) return { text: "старая", icon: waningSmall };
   return { text: "новолуние", icon: newMoon };
 }
